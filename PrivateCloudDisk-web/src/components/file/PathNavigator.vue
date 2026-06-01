7b23c7101606b1270c868443c795adeea1752a0d
spade-kk <113084528+spade-kk@users.noreply.github.com>
2026-06-02 02:29:08 +0800

    On branch main Changes to be committed: 	modified:   PrivateCloudDisk-web/src/components/file/PathNavigator.vue
    Untracked files:
    	.DS_Store
    	.vscode/
    	PrivateCloudDisk-gateway-service/.DS_Store
    	PrivateCloudDisk-gateway-service/.gitattributes
    	PrivateCloudDisk-gateway-service/.gitignore
    	PrivateCloudDisk-gateway-service/src/main/resources/keys/
    	PrivateCloudDisk-platform-service/.DS_Store
    	PrivateCloudDisk-platform-service/.gitattributes
    	PrivateCloudDisk-platform-service/.gitignore
    	PrivateCloudDisk-platform-service/src/.DS_Store
    	PrivateCloudDisk-platform-service/src/main/.DS_Store
    	PrivateCloudDisk-platform-service/src/main/resources/keys/
    	PrivateCloudDisk-shortage-service/.DS_Store
    	PrivateCloudDisk-shortage-service/__pycache__/
    	PrivateCloudDisk-shortage-service/private_key.pem
    	PrivateCloudDisk-shortage-service/public_key.pem
    	PrivateCloudDisk-web/.DS_Store
    	PrivateCloudDisk-web/.gitignore
    	PrivateCloudDisk-web/dist/
    	PrivateCloudDisk-web/index.html
    	PrivateCloudDisk-web/package-lock.json
    	PrivateCloudDisk-web/src/.DS_Store
    	Uploads/
    	index.html
    	index2.html
    	"\347\247\201\346\234\211\344\272\221\347\275\221\347\233\230\347\256\241\347\220\206\347\263\273\347\273\237-\346\226\207\344\273\266\344\270\212\344\274\240-\345\210\207\347\211\207\347\275\221\346\240\274\347\211\210.html"
    	"\347\247\201\346\234\211\344\272\221\347\275\221\347\233\230\347\256\241\347\220\206\347\263\273\347\273\237-\346\226\207\344\273\266\344\270\212\344\274\240-\350\277\233\345\272\246\346\235\241\347\211\210.html"
    	"\347\247\201\346\234\211\344\272\221\347\275\221\347\233\230\347\256\241\347\220\206\347\263\273\347\273\237-\346\226\207\344\273\266\345\234\250\347\272\277\351\242\204\350\247\210.html"
    	"\347\247\201\346\234\211\344\272\221\347\275\221\347\233\230\347\256\241\347\220\206\347\263\273\347\273\237-\346\226\207\344\273\266\345\261\225\347\244\272.html"
    	"\347\247\201\346\234\211\344\272\221\347\275\221\347\233\230\347\256\241\347\220\206\347\263\273\347\273\237-\350\236\215\345\220\210\345\212\237\350\203\275.html"

--- PrivateCloudDisk-web/src/components/file/PathNavigator.vue
+++ PrivateCloudDisk-web/src/components/file/PathNavigator.vue
@@ -3,14 +3,14 @@
     <button @click="$emit('home')" class="text-primary hover:text-primary/80 flex items-center whitespace-nowrap">
       <i class="fa fa-home mr-1"></i><span>我的网盘</span>
     </button>
-    <template v-for="(item, idx) in pathStack" :key="item.node_id">
+    <div v-for="(item, idx) in pathStack" :key="item.node_id">
       <span class="text-neutral-300"><i class="fa fa-angle-right"></i></span>
       <div class="flex-shrink-0">
         <a @click="idx < pathStack.length - 1 ? $emit('navigate', item) : null" :class="idx === pathStack.length - 1 ? 'text-neutral-700 font-medium' : 'text-primary hover:text-primary/80 cursor-pointer'">
           {{ item.node_name }}
         </a>
       </div>
-    </template>
+    </div>
   </div>
 </template>
 
